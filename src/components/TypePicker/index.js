7d6e0f007adb561c98b726f1b091957daddb9f40
Vinicius Leite <viniciusleite@brq.com>
2023-11-22 19:12:23 -0300

    build: configuracao de build

--- src/components/TypePicker/index.js
+++ src/components/TypePicker/index.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
-
+import React from "react"
+import { View } from "react-native"
+import { Picker } from "@react-native-picker/picker"
 
 export default function TypePicker({ selectedType, setSelectedType, width }) {
-    return (
-        <Picker
-            style={{ color: '#fff', width: width || '40%' }}
-            dropdownIconColor={'#fff'}
-            dropdownIconRippleColor={'#fff'}
-            selectedValue={selectedType}
-            onValueChange={(itemValue, itemIndex) =>
-                setSelectedType(itemValue)
-            }>
-            <Picker.Item label="Cabelo" value="cabelo" />
-            <Picker.Item label="Unha" value="unha" />
-            <Picker.Item label="Cílios" value="cilios" />
-        </Picker>
-    );
+	return (
+		<Picker
+			style={{ color: "#fff", width: width || "40%" }}
+			dropdownIconColor={"#fff"}
+			dropdownIconRippleColor={"#fff"}
+			selectedValue={selectedType}
+			onValueChange={(itemValue, itemIndex) =>
+				setSelectedType(itemValue)
+			}>
+			<Picker.Item label="Cabelo" value="cabelo" />
+			<Picker.Item label="Unha" value="unha" />
+			<Picker.Item label="Cílios" value="cilios" />
+		</Picker>
+	)
 }
