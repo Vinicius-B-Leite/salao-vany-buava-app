bf06d0e74e9a45d35c60d905bf44f141fe49ea94
Vinicius Leite <viniciusleite@brq.com>
2023-11-22 10:25:49 -0300

    feat: adicionando notificacao para o dia da cliente marcada

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import AuthContextProvider from './src/contexts/auth';
-import Routes from './src/routes';
-import Login from './src/screens/Login';
-import { StatusBar } from 'react-native';
+import "react-native-gesture-handler"
+import React, { useEffect } from "react"
+import AuthContextProvider from "./src/contexts/auth"
+import Routes from "./src/routes"
+import Login from "./src/screens/Login"
+import { StatusBar } from "react-native"
 
+import * as Notifications from "expo-notifications"
 
+Notifications.setNotificationHandler({
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: false,
+		shouldSetBadge: false,
+	}),
+})
 
 export default function App() {
-  return (
-    <AuthContextProvider>
-      <StatusBar/>
-      <Routes />
-    </AuthContextProvider>
-  );
+	return (
+		<AuthContextProvider>
+			<StatusBar />
+			<Routes />
+		</AuthContextProvider>
+	)
 }
--- src/screens/Schedule/index.js
+++ src/screens/Schedule/index.js
@@ -1,136 +1,205 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from "react"
 
-import { ActivityIndicator, KeyboardAvoidingView, Platform, TouchableOpacity } from 'react-native';
-import * as S from './styles'
+import {
+	ActivityIndicator,
+	KeyboardAvoidingView,
+	Platform,
+	TouchableOpacity,
+} from "react-native"
+import * as S from "./styles"
 
-import DatePicker from '../../components/DatePicker'
-import HourPicker from '../../components/HourPicker';
-import TypePicker from '../../components/TypePicker';
-import ProcedingsModal from '../../components/ProcedingsModal';
+import DatePicker from "../../components/DatePicker"
+import HourPicker from "../../components/HourPicker"
+import TypePicker from "../../components/TypePicker"
+import ProcedingsModal from "../../components/ProcedingsModal"
 
-import { Entypo } from '@expo/vector-icons';
-import { Feather } from '@expo/vector-icons';
+import { Entypo } from "@expo/vector-icons"
+import { Feather } from "@expo/vector-icons"
 
-import { db, dbRef } from '../../service/firebase'
-import { child, get, push, ref, set } from 'firebase/database';
+import { db, dbRef } from "../../service/firebase"
+import { child, get, push, ref, set } from "firebase/database"
 
-import { format } from 'date-fns';
-import { useFocusEffect } from '@react-navigation/native';
-
-
-
-
-
+import { format } from "date-fns"
+import { useFocusEffect } from "@react-navigation/native"
+import * as Notifications from "expo-notifications"
 
 export default function Schedule() {
+	const [showDatePicker, setShowDatePicker] = useState(false)
+	const [showHourPicker, setShowHourPicker] = useState(false)
+	const [proceddingsModalVisible, setProccedingsModalVisible] =
+		useState(false)
+	const [errorMessage, setErrorMessage] = useState()
+	const [loading, setLoading] = useState(false)
 
-  const [showDatePicker, setShowDatePicker] = useState(false)
-  const [showHourPicker, setShowHourPicker] = useState(false)
-  const [proceddingsModalVisible, setProccedingsModalVisible] = useState(false)
-  const [errorMessage, setErrorMessage] = useState()
-  const [loading, setLoading] = useState(false)
+	const [clientName, setClientName] = useState()
+	const [selectedType, setSelectedType] = useState("cabelo")
+	const [totalValue, setTotalValue] = useState(0)
+	const [date, setDate] = useState(new Date())
+	const [hour, setHour] = useState(new Date())
+	const [selectedProceedings, setSelectedProceedings] = useState([])
 
-  const [clientName, setClientName] = useState()
-  const [selectedType, setSelectedType] = useState('cabelo')
-  const [totalValue, setTotalValue] = useState(0)
-  const [date, setDate] = useState(new Date())
-  const [hour, setHour] = useState(new Date())
-  const [selectedProceedings, setSelectedProceedings] = useState([])
+	function submit() {
+		setLoading(true)
+		if (
+			!clientName ||
+			!selectedType ||
+			!totalValue ||
+			!date ||
+			!hour ||
+			selectedProceedings.length < 1
+		) {
+			setErrorMessage("Preencha todos os campos")
+			setLoading(false)
+			return
+		}
 
+		setErrorMessage("")
+		let newKey = push(dbRef)
 
-  function submit() {
-    setLoading(true)
-    if (!clientName || !selectedType || !totalValue || !date || !hour || selectedProceedings.length < 1) {
-      setErrorMessage('Preencha todos os campos')
-      setLoading(false)
-      return
-    }
+		let keysSelected = selectedProceedings.map((item) => item.id)
 
-    setErrorMessage('')    
-    let newKey = push(dbRef)
+		set(newKey, {
+			cliente: clientName,
+			data: format(date, "dd/MM/yyyy"),
+			hora: format(hour, "HH:mm"),
+			tipo: selectedType,
+			valor: totalValue,
+			id: newKey.toString().slice(60, newKey.length),
+			procedimento: keysSelected,
+		})
+			.then(async () => {
+				let dateToDisplayNotification = new Date(date)
+				dateToDisplayNotification.setHours(7)
+				await Notifications.scheduleNotificationAsync({
+					content: {
+						title: "Vany, você tem cliente hoje!!",
+						body: `A cliente ${clientName} está marcada para hoje às ${format(
+							date,
+							"HH:mm"
+						)}`,
+						data: { data: "goes here" },
+					},
+					trigger: {
+						date: dateToDisplayNotification,
+					},
+				})
+			})
+			.finally(() => {
+				setLoading(false)
+				setClientName("")
+				setTotalValue("")
+				setSelectedProceedings([])
+			})
+	}
 
-    let keysSelected = selectedProceedings.map(item => (item.id))
+	return (
+		<KeyboardAvoidingView
+			style={{ flex: 1 }}
+			behavior={Platform.OS === "ios" ? "padding" : "height"}
+			enabled={false}>
+			<S.Container>
+				<S.Main>
+					{errorMessage && <S.Error>{errorMessage}</S.Error>}
+					<S.Input
+						placeholder="Nome da cliente"
+						value={clientName}
+						onChangeText={(txt) => setClientName(txt)}
+					/>
 
-    set(newKey, {
-      cliente: clientName,
-      data: format(date, 'dd/MM/yyyy'),
-      hora: format(hour, 'HH:mm'),
-      tipo: selectedType,
-      valor: totalValue,
-      id: newKey.toString().slice(60, newKey.length),
-      procedimento: keysSelected
-    }).finally(() => {
-      setLoading(false)
-      setClientName('')
-      setTotalValue('')
-      setSelectedProceedings([])
-    })
+					<S.Row>
+						<TypePicker
+							selectedType={selectedType}
+							setSelectedType={setSelectedType}
+						/>
+						<S.Input
+							placeholder="Valor"
+							width="50%"
+							value={totalValue}
+							onChangeText={(txt) => setTotalValue(txt)}
+							keyboardType="numeric"
+						/>
+					</S.Row>
 
-  }
+					<S.Row>
+						<S.Row width="45%">
+							<TouchableOpacity
+								onPress={() =>
+									setShowDatePicker(!showDatePicker)
+								}>
+								<Entypo
+									name="calendar"
+									size={24}
+									color="#fff"
+								/>
+							</TouchableOpacity>
+							<S.Input
+								placeholder="Data"
+								width="70%"
+								value={format(date, "dd/MM/yyyy")}
+								editable={false}
+							/>
+							{showDatePicker && (
+								<DatePicker
+									date={date}
+									setDate={setDate}
+									setShowDatePicker={setShowDatePicker}
+								/>
+							)}
+						</S.Row>
 
-  return (
-    <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : 'height'} enabled={false}>
-      <S.Container>
-        <S.Main>
-          {errorMessage && <S.Error>{errorMessage}</S.Error>}
-          <S.Input placeholder='Nome da cliente' value={clientName} onChangeText={txt => setClientName(txt)} />
+						<S.Row width="45%">
+							<TouchableOpacity
+								onPress={() =>
+									setShowHourPicker(!showHourPicker)
+								}>
+								<Feather name="clock" size={24} color="#fff" />
+							</TouchableOpacity>
+							{showHourPicker && (
+								<HourPicker
+									hour={hour}
+									setHour={setHour}
+									setShowHourPicker={setShowHourPicker}
+								/>
+							)}
+							<S.Input
+								width="70%"
+								value={format(hour, "HH:mm")}
+								editable={false}
+							/>
+						</S.Row>
+					</S.Row>
 
-          <S.Row>
-            <TypePicker selectedType={selectedType} setSelectedType={setSelectedType} />
-            <S.Input
-              placeholder='Valor'
-              width='50%'
-              value={totalValue}
-              onChangeText={txt => setTotalValue(txt)}
-              keyboardType="numeric" />
-          </S.Row>
+					<TouchableOpacity
+						onPress={() => setProccedingsModalVisible(true)}>
+						<S.Row>
+							<S.ScheduleTitle>Procedimentos</S.ScheduleTitle>
+							<Entypo
+								name="triangle-down"
+								size={24}
+								color="#fff"
+							/>
+							<S.Underline />
+						</S.Row>
+					</TouchableOpacity>
+					<ProcedingsModal
+						setProccedingsModalVisible={setProccedingsModalVisible}
+						proceddingsModalVisible={proceddingsModalVisible}
+						type={selectedType}
+						selectedProceedings={selectedProceedings}
+						setSelectedProceedings={setSelectedProceedings}
+					/>
 
-
-          <S.Row>
-
-            <S.Row width="45%">
-              <TouchableOpacity onPress={() => setShowDatePicker(!showDatePicker)}>
-                <Entypo name="calendar" size={24} color="#fff" />
-              </TouchableOpacity>
-              <S.Input
-                placeholder="Data"
-                width="70%"
-                value={format(date, 'dd/MM/yyyy')}
-                editable={false} />
-              {showDatePicker && <DatePicker date={date} setDate={setDate} setShowDatePicker={setShowDatePicker} />}
-            </S.Row>
-
-            <S.Row width="45%">
-              <TouchableOpacity onPress={() => setShowHourPicker(!showHourPicker)}>
-                <Feather name="clock" size={24} color="#fff" />
-              </TouchableOpacity>
-              {showHourPicker && <HourPicker hour={hour} setHour={setHour} setShowHourPicker={setShowHourPicker} />}
-              <S.Input width="70%" value={format(hour, 'HH:mm')} editable={false} />
-            </S.Row>
-
-          </S.Row>
-
-
-          <TouchableOpacity onPress={() => setProccedingsModalVisible(true)}>
-            <S.Row>
-              <S.ScheduleTitle>Procedimentos</S.ScheduleTitle>
-              <Entypo name="triangle-down" size={24} color="#fff" />
-              <S.Underline />
-            </S.Row>
-          </TouchableOpacity>
-          <ProcedingsModal
-            setProccedingsModalVisible={setProccedingsModalVisible}
-            proceddingsModalVisible={proceddingsModalVisible}
-            type={selectedType}
-            selectedProceedings={selectedProceedings}
-            setSelectedProceedings={setSelectedProceedings} />
-
-
-          <S.Button onPress={() => submit()}>
-            <S.ButtonText>{loading ? <ActivityIndicator color='#fff' size={24} /> : 'Agendar'}</S.ButtonText>
-          </S.Button>
-        </S.Main>
-      </S.Container>
-    </KeyboardAvoidingView>
-  );
+					<S.Button onPress={() => submit()}>
+						<S.ButtonText>
+							{loading ? (
+								<ActivityIndicator color="#fff" size={24} />
+							) : (
+								"Agendar"
+							)}
+						</S.ButtonText>
+					</S.Button>
+				</S.Main>
+			</S.Container>
+		</KeyboardAvoidingView>
+	)
 }
